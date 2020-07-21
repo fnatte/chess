bccb9acaa6779182268327d9f7b49de88c9fc173
Matteus <matteus92@gmail.com>
2020-07-21 13:08:06 +0200

    clean gh-pages

--- src/engine/command.js
+++
@@ -1,9 +0,0 @@
-import { makeMove, nextTurn } from "./chess";
-
-export const moveCommand = (fromIndex, toIndex) => (game) =>
-  makeMove(game, fromIndex, toIndex);
-
-/**
- * Invalid chess command really. Only used for debugging.
- */
-export const passCommand = () => (game) => nextTurn(game);
--- src/engine/new-game.js
+++
@@ -1,9 +0,0 @@
-import { createBoard } from "./board";
-import { PieceColor } from "./constants";
-
-export default function newGame() {
-  return {
-    board: createBoard(),
-    turn: PieceColor.white,
-  };
-}
